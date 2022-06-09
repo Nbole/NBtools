163c52ec28c8fe04bca9eebf717dbd51534055b3
Nicolas <nicolas.bole@hotmail.com>
2022-06-09 16:53:21 -0300

    change method

--- NBToolsLibrary/src/main/java/com/example/nbtoolslibrary/Util.kt
+++ NBToolsLibrary/src/main/java/com/example/nbtoolslibrary/Util.kt
@@ -2,9 +2,17 @@ package com.example.nbtoolslibrary
 
 import android.util.Log
 
-fun <T> getTime(action: () -> T, message: String): T {
+/**
+ * Estimate time in milliseconds and show in Log, [tag] and [message] are optionals Strings you
+ * can use to see the result in your log.
+ */
+fun <T> getMilliseconds(
+    tag: String? = "EstimatedTime",
+    message: String? = "value:",
+    action: () -> T
+): T {
     val currentTime = System.currentTimeMillis()
-    val r = action.invoke()
-    Log.d("CalculateTime", "${(System.currentTimeMillis()) - currentTime}")
-    return r
+    val returnValue = action.invoke()
+    Log.d(tag, "$message ${(System.currentTimeMillis()) - currentTime}")
+    return returnValue
 }
